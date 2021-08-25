42055f73c7080ada7466201bdd970de32c330bca
cwx96 <372016893@qq.com>
2021-08-25 13:15:06 +0800

    repo

---
+++ src/test/java/life/chen/community/CoummunityApplicationTests.java
@@ -0,0 +1,13 @@
+package life.chen.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoummunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
